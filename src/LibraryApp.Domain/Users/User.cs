b30eed49b7b01b593475ef11eeda326637f99902
Marko M <misimovicmarko@gmail.com>
2025-11-04 20:40:18 +0100

    Add User entity

---
+++ src/LibraryApp.Domain/Users/User.cs
@@ -0,0 +1,44 @@
+﻿namespace LibraryApp.Domain.Users;
+
+public class User
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string Password { get; private set; }
+    public UserRole Role { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public bool IsActive { get; private set; }
+
+    private User(string name, string email, string password, UserRole role)
+    {
+        if(string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Name cannot be empty.", nameof(name));
+        if (string.IsNullOrWhiteSpace(email))
+            throw new ArgumentException("Email cannot be empty.", nameof(email));
+        if (string.IsNullOrWhiteSpace(password))
+            throw new ArgumentException("Password cannot be empty.", nameof(password));
+
+        Id = Guid.NewGuid();
+        Name = name;
+        Email = email;
+        Password = password;
+        Role = role;
+        CreatedAt = DateTime.Now;
+        IsActive = true;
+    }
+
+    public static User Create(string name, string email, string password, UserRole role = UserRole.Member)
+    {
+        return new User(name, email, password, role);
+    }
+
+    public void Deactivate()
+    {
+        if (!IsActive)
+            throw new InvalidOperationException("User is already inactive.");
+
+        IsActive = false;
+    }
+
+}
